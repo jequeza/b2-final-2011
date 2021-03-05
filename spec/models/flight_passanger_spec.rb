7023805794d5c90f8ac0d9eadd229f45465f0f71
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-05 10:22:07 -0600

    Add test for assossiation relationships of fligh_passanger.

--- spec/models/flight_passanger_spec.rb
+++ spec/models/flight_passanger_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe FlightPassanger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to :flight}
+it {should belong_to :passanger}
 end
