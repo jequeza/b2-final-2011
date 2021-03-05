0a0a762f8748eb4196da5310da8507dd2983bb15
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-05 10:19:08 -0600

    Add test for assossiation relationships of passanger.

--- spec/models/passanger_spec.rb
+++ spec/models/passanger_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Passanger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many :flight_passangers}
+  it {should have_many(:flights).through(:flight_passangers)}
 end
