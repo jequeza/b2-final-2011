d1065d11dfd80609d9fe5ba4eb53c12b097ee81b
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-05 10:36:38 -0600

    Crate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flight.destroy_all
+Passanger.destroy_all
+FlightPassanger.destroy_all
+
+
+vacation = Flight.create!(number: "1234", date: "06/12/2021", departure_city: "Chicago", arrival_city: "Cancun")
+work = Flight.create!(number: "3456", date: "08/03/2020", departure_city: "Miami", arrival_city: "Los Angeles")
+
+jack = vacation.passangers.create!(name: "Jack Wilson", age: 23)
+sam = vacation.passangers.create!(name: "Sam Wilson", age: 28)
+jordan = vacation.passangers.create!(name: "Jordan Wilson", age: 21)
+steve = vacation.passangers.create!(name: "Steve Wilson", age: 16)
+
+phil = work.passangers.create!(name: "Phil Smith", age: 45)
+richard = work.passangers.create!(name: "Richard Low", age: 39)
+carla = work.passangers.create!(name: "Carla Ramos", age: 40)
+angelica = work.passangers.create!(name: "Angelica B", age: 35)
+
+
