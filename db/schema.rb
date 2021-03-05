90af355ddf55d0a987e45a6a9797f89ba8e60a1a
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-05 12:34:44 -0600

    Add patients table and files.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_160938) do
+ActiveRecord::Schema.define(version: 2021_03_05_183404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2021_03_05_160938) do
   end
 
   create_table "passangers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
