3055de462062a0ca8858e0c189377a9194f6daa0
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-05 11:16:06 -0600

    Add method to flight model, all test pass.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight < ApplicationRecord
   has_many :flight_passangers
   has_many :passangers, through: :flight_passangers
+
+
+  def self.flights_by_departure_city
+    select("flights.*").order(:departure_city)
+  end
 end
